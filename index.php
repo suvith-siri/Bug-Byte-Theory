425f9d1fcbbfd2aafba228556f4cfb6190282fa5
suvith-siri <77716573+suvith-siri@users.noreply.github.com>
2021-02-27 12:50:57 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Our Planet Info</title>
+
+	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Balsamiq+Sans:ital@1&family=Zilla+Slab&display=swap" rel="stylesheet">
+<body>
+
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark"> 
+  <a class="navbar-brand" href="#">Our Planet Info</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="deforest.html">Deforestation</a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link" href="main1.html">Agriculture</a>
+      </li>
+
+
+
+      <li class="nav-item">
+        <a class="nav-link" href="main (2).html">Sustainable Development</a>
+      </li>
+
+
+
+      <li class="nav-item">
+        <a class="nav-link" href="education.html">Education</a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link" href="waterfood (1).html">Food And Water Wastage</a>
+      </li>
+
+
+
+
+      
+
+
+      <li class="nav-item">
+        <a class="nav-link" href="homeabout.html">About</a>
+      </li>
+
+
+      
+
+
+
+
+
+      
+      
+    </ul>
+    
+  </div>
+</nav>
+
+
+
+<div id="demo" class="carousel slide" data-ride="carousel">
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="cc1.jpg" alt="Los Angeles" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>There is a sufficiency in The World for man's need but not for man's greed</h3>
+        <p>Mahatma Gandhi</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="cc2.jpg" alt="Chicago" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>You must be the change you wish to see in The World.</h3>
+        <p>Mahatma Gandhi</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="cc3.jpg" alt="New York" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>Culture is the windening of the mind and of the spirit.</h3>
+        <p>Jawaharlal Nehru</p>
+      </div>   
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+
+
+
+
+	
+</section>
+
+
+
+
+
+<section class="my-5">
+	<div class="py-5">
+		<h2 class="text-center">Contact Us</h2>
+	</div>
+	<div class="w-50 m-auto">
+		<form action="userinfo.php" method="post">
+			<div class="form-group">
+				<label>UserName</label>
+				<input type="text" name="user" autocomplete="off" class="form-control">
+			</div>
+
+			<div class="form-group">
+				<label>Email</label>
+				<input type="text" name="user" autocomplete="off" class="form-control">
+			</div>
+
+			<div class="form-group">
+				<label>Mobile No.</label>
+				<input type="text" name="user" autocomplete="off" class="form-control">
+			</div>
+
+			<div class="form-group">
+				<label>Comment</label>
+				<textarea class="form-control"></textarea>
+			</div>
+			<button type="submit" class="btn btn-primary">Submit</button>
+
+
+		</form>
+	</div>
+</section>
+
+<footer>
+	<h6 class="p-3 bg-dark text-white text-center">@OurPlanetInfo</h6>
+</footer>
+
+
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+</body>
+</html>
